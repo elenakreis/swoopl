edd03ba5e6f75c695a41e2518494679a911c8e50
Vloon <pleunscholten@hotmail.com>
2019-03-20 14:26:10 +0100

    added comments specifying input-output.

--- Prolog Assignment/implementation.pl
+++ Prolog Assignment/implementation.pl
@@ -1,5 +1,5 @@
 :- [diagnosis].
-%%% intersect(L1,L2,X) unifies X with the interesection between L1 and L2.
+%%% intersect(L1,L2,X) unifies X with the intersection between L1 and L2.
   intersect([], _, []).
   %If H1 is in L2
   intersect([H1|L1], L2, [H1|ITC]) :-
@@ -10,13 +10,16 @@
     intersect(L1,L2,ITC).
 
 % Helper function for label.
+%%% getLabel(F, Hn, Label) unifies Label wit the first item in F for which holds that the intersection between Hn and S\in F == [].
 getLabel([HeadF|TailF], Hn, HeadF):- % base case
   intersect(HeadF, Hn, []) % take first item of F as label if intersection is empty
   .
 getLabel([HeadF|TailF], Hn, Label):-
   getLabel(TailF, Hn, Label) % If head intersection was not empty, try next element
   .
+
 % Helper function for children.
+%%% getChildren(F,prevHn, Label, Children) unifies Children with the children of a node with set of edgelabels Hn.
 getChildren(_,_,[],[]).
 getChildren(F, Hn, [Head|Tail], [HeadC|TailC]) :-
     makeHittingTree(F, [Head|Hn] , HeadC),
@@ -24,6 +27,7 @@ getChildren(F, Hn, [Head|Tail], [HeadC|TailC]) :-
     .
 % Generates a hitting tree HT from a set of conflict sets.
 % Using Hn as an additional parameter is not necessary (it could be done with pattern matching inside the node), but it is much more readible this way so we decided to stick with it.
+%%% makeHittingTree(F,Hn,Tree) unifies Tree with the hitting tree of F.
 makeHittingTree([],_,node([], check, Hn])).
 makeHittingTree([S|TailF], Hn, HT) :- % base case
   intersect(S, Hn, I), I \= [], % S intersection Hn is not empty
@@ -33,7 +37,6 @@ makeHittingTree(F, Hn, node(Children, Label, Hn)) :-
   getLabel(F, Hn, Label),
   getChildren(F, Hn, Label, Children)
   .
-
 % Generates a hitting tree HT for a diagnostic problem (SD, COMP, OBS).
 makeHittingTree(SD, COMP, OBS, HT) :-
   getF(SD, COMP, OBS, F), % TODO
