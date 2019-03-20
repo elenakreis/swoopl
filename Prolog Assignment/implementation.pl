0f1673dfa59ddaa773b3f0f48c0935cc8fb46c8e
Elena <elenakreis@web.de>
2019-03-20 14:17:25 +0100

    Implemented getLabel, changed makeHittingTree base case

--- Prolog Assignment/implementation.pl
+++ Prolog Assignment/implementation.pl
@@ -1,3 +1,4 @@
+:- [diagnosis].
 %%% intersect(L1,L2,X) unifies X with the interesection between L1 and L2.
   intersect([], _, []).
   %If H1 is in L2
@@ -8,6 +9,13 @@
   intersect([_|L1] , L2, ITC) :-
     intersect(L1,L2,ITC).
 
+% Helper function for label.
+getLabel([HeadF|TailF], Hn, HeadF):- % base case
+  intersect(HeadF, Hn, []) % take first item of F as label if intersection is empty
+  .
+getLabel([HeadF|TailF], Hn, Label):-
+  getLabel(TailF, Hn, Label) % If head intersection was not empty, try next element
+  .
 % Helper function for children.
 getChildren(_,_,[],[]).
 getChildren(F, Hn, [Head|Tail], [HeadC|TailC]) :-
@@ -16,13 +24,13 @@ getChildren(F, Hn, [Head|Tail], [HeadC|TailC]) :-
     .
 % Generates a hitting tree HT from a set of conflict sets.
 % Using Hn as an additional parameter is not necessary (it could be done with pattern matching inside the node), but it is much more readible this way so we decided to stick with it.
-makeHittingTree([],_,_).
-makeHittingTree([HeadF|TailF], Hn, node([], check, Hn])) :-
-  intersect(HeadF, Hn, S), S \= [],
-  makeHittingTree(TailF, Hn, node([], check, Hn))
+makeHittingTree([],_,node([], check, Hn])).
+makeHittingTree([S|TailF], Hn, HT) :- % base case
+  intersect(S, Hn, I), I \= [], % S intersection Hn is not empty
+  makeHittingTree(TailF, Hn, HT)
   .
 makeHittingTree(F, Hn, node(Children, Label, Hn)) :-
-  Label = ..., % TODO
+  getLabel(F, Hn, Label),
   getChildren(F, Hn, Label, Children)
   .
 
