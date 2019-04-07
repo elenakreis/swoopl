5d4ce56b3b7919ece8ff16caf6ff2d496e71ff29
Vloon <pleunscholten@hotmail.com>
2019-04-07 15:51:30 +0200

    Slightly changed the documentation for consistency.

--- Prolog Assignment/implementation.pl
+++ Prolog Assignment/implementation.pl
@@ -1,18 +1,18 @@
 :- [diagnosis].
-% Helper function for children.
-%%% getChildren(DP ,prevHn, Label, Children) unifies Children with the children of a node with set of edgelabels Hn.
+% Helper function for creating the children in the hitting set.
+% getChildren(DP ,prevHn, Label, Children) unifies Children with the children of a node with set of edgelabels Hn.
 getChildren(_,_,[],[]).
 getChildren(DP, Hn, [Head|Tail], [HeadC|TailC]) :-
     makeHittingTree(DP, [Head|Hn] , HeadC),
     getChildren(DP, Hn, Tail, TailC)
     .
-%%% makeHittingTree([SD, COMP, OBS], Hn, Tree) unifies Tree with the hitting tree of the diagnostic problem [SD, COMP, OBS].
+% makeHittingTree([SD, COMP, OBS], Hn, Tree) unifies Tree with the hitting tree of the diagnostic problem [SD, COMP, OBS].
 % Note: Using Hn as an additional parameter is not necessary (it could be done with pattern matching inside the node), but it is much more readible this way so we decided to stick with it.
-makeHittingTree([SD, COMP, OBS], Hn, node(Children, S, Hn)) :- % node: list of Children, label S and list of edge labels Hn
+makeHittingTree([SD, COMP, OBS], Hn, node(Children, S, Hn)) :- % node: list of Children, label S and list of edge labels Hn.
   tp(SD, COMP, OBS, Hn, S),
   getChildren([SD, COMP, OBS], Hn, S, Children)
   .
-makeHittingTree(_, Hn, node([], check, Hn)). % if tp fails go here
+makeHittingTree(_, Hn, node([], check, Hn)). % if tp fails, go here.
 
 % Generates a hitting tree HT for a diagnostic problem (SD, COMP, OBS).
 makeHittingTree(SD, COMP, OBS, HT) :-
@@ -20,7 +20,7 @@ makeHittingTree(SD, COMP, OBS, HT) :-
   .
 
 % -------------------------------------------------------------------------
-% Helper function to gather diagnoses from each child
+% Helper function to gather diagnoses from each child.
 gatherChildDiagnoses([], []).
 gatherChildDiagnoses([HeadC|TailC], D) :-
   gatherDiagnoses(HeadC, HeadCD),
@@ -28,7 +28,7 @@ gatherChildDiagnoses([HeadC|TailC], D) :-
   append(HeadCD, TailCD, D)
   .
 
-% Function for getting all hitting sets of a hitting tree
+% Function for getting all hitting sets of a hitting tree.
 gatherDiagnoses(node(_, check, Hn), [Hn]).
 gatherDiagnoses(node(Children, _, _), D) :-
   gatherChildDiagnoses(Children, D)
@@ -57,13 +57,13 @@ insert(X,[Y|T],[X,Y|T]):-
 insert(X,[],[X]).
 
 %---------------------------------------------------
-% isSuperset(Set, SetofSets) determines whether Set is a superset of any set in SetofSets
+% isSuperset(Set, SetofSets) determines whether Set is a superset of any set in SetofSets.
 ~isSuperset(_, []).
 isSuperset(D, [Head|Tail]) :-
   subset(Head, D); % D is superset of Head
   isSuperset(D, Tail)
   .
-% getMD(D, MD). Helper function for getMinimalDiagnoses. Removes all supersets from D, when the sets in D are ordered by descending length
+% getMD(D, MD). Helper function for getMinimalDiagnoses. Removes all supersets from D, if the sets in D are ordered by descending length.
 getMD([],[]).
 getMD([HeadD|TailD], MD):-
   getMD(TailD, TailMD),
@@ -80,13 +80,13 @@ getMD([HeadD|TailD], MD):-
 % Removes all supersets of D to get MD.
 % Sorts the sets in D by length, and then reverses them to have them in descending order.
 getMinimalDiagnoses(D, MD) :-
-  insert_sort(D, DSort), % Sort by length of sublists
-  reverse(DSort, DSortReverse), % Put longest lists in front
+  insert_sort(D, DSort), % Sort by length of sublists.
+  reverse(DSort, DSortReverse), % Put longest lists in front.
   getMD(DSortReverse, MD)
   .
 
 % ------------------------------------------------------------------------
-% Main predicate that takes a diagnostic problem and returns the minimal diagnoses MD
+% Main predicate that takes a diagnostic problem and returns the minimal diagnoses MD.
 solveDP(SD, COMP, OBS, MD):-
   makeHittingTree(SD, COMP, OBS, HT),
   gatherDiagnoses(HT, D),
